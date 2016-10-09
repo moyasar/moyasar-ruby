24cafd425664bf5f38d15a7aa879deb87628e19b
Abdulaziz AlShetwi <ecleeld@gmail.com>
2016-10-09 23:12:03 +0300

    Add guard and minitest reporters in development deps

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in moyasar.gemspec
 gemspec
+
+group :development do
+  gem 'minitest-reporters'
+  gem 'guard' # NOTE: this is necessary in newer versions
+  gem 'guard-minitest'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'moyasar'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+TEST_KEY = ENV.fetch('MOYASAR_RUBY_WRAPPER_TEST_PRIVATE_KEY')
+EMPTY_ACCOUNT_TEST_KEY = ENV.fetch('MOYASAR_RUBY_WRAPPER_EMPTY_ACCOUNT_TEST_KEY')
