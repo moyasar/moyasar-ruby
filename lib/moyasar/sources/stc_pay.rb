4fa65372ea057ecc7beac798a0e4751b1e5a0527
Abdulaziz Alshetwi <ecleeld@gmail.com>
2020-07-14 10:48:06 +0300

    Add ApplePay and STCPay sources

---
+++ lib/moyasar/sources/stc_pay.rb
@@ -0,0 +1,10 @@
+module Moyasar
+  class STCPay < Source
+    attr_reader :mobile, :reference_number, :branch, :cashier, :reference_number, :transaction_url, :message
+
+    def ==(other)
+      return unless other.instance_of? STCPay
+      [:mobile, :reference_number, :branch, :cashier, :reference_number, :transaction_url, :message].all? { |attr| self.send(attr) == other.send(attr) }
+    end
+  end
+end
